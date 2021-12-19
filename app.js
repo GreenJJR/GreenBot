a711028d10b319b694f7fb567dc4ba6921f9f0e2
초록장조림 <84648358+GreenJJR@users.noreply.github.com>
2021-12-19 14:32:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', async message => {
-  if (message.content.startsWith('!reply')) {
-    message.reply('Hey'); //Line (Inline) Reply with mention
+client.on('messageCreate', msg => { // message 이벤트시 msg (Discord.Message) 매개변수를 받고 실행할 함수
+  if (msg.content === 'ping') { // Discord.Message 객체의 content 프로퍼티가 'ping' 일 때
+    msg.reply('Pong!'); // reply 는 멘션 + , msg 로 출력됩니다.
   }
 });
 
