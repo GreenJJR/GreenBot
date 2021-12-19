5df97ba2a684175242a911d9843f6acb2c1decc2
초록장조림 <84648358+GreenJJR@users.noreply.github.com>
2021-12-19 15:54:43 +0900

    Fixed Error

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', msg => { // message 이벤트시 msg (Discord.Message) 매개변수를 받고 실행할 함수
+client.on('message', msg => { // message 이벤트시 msg (Discord.Message) 매개변수를 받고 실행할 함수
   if (msg.content === '야') { // Discord.Message 객체의 content 프로퍼티가 'ping' 일 때
     msg.reply('호!'); // reply 는 멘션 + , msg 로 출력됩니다.
   }
