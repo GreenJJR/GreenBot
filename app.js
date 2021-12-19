40d76bcc2cd5d4833e2bb3c0dea056ef1555a1b8
초록장조림 <84648358+GreenJJR@users.noreply.github.com>
2021-12-19 16:06:36 +0900

    Rock Sissors Paper!

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => { // message 이벤트시 msg (Discord.Message) 매개변수를 받고 실행할 함수
-  if (msg.content === '야') { // Discord.Message 객체의 content 프로퍼티가 'ping' 일 때
-    msg.reply('호!'); // reply 는 멘션 + , msg 로 출력됩니다.
+  if (msg.content === "야") { // Discord.Message 객체의 content 프로퍼티가 'ping' 일 때
+    msg.reply("호!"); // reply 는 멘션 + , msg 로 출력됩니다.
+  }
+  
+  if (msg.content === "가위" || "바위" || "보") {
+    const human = msg.content;
+    const list = ["가위", "바위", "보"];
+    const random = Math.random() * 3;
+
+    msg.reply(random);
   }
 });
 
