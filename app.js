f940259d81d81de8392eee1a824eed62fa7ac494
초록장조림 <84648358+GreenJJR@users.noreply.github.com>
2021-12-20 18:32:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -68,19 +68,23 @@ client.on('message', msg => { // message 이벤트시 msg (Discord.Message) 매�
     if (user.id){
       const embed = new Discord.MessageEmbed()
       .setAuthor("초록봇", "https://yt3.ggpht.com/J8ldYsbmlo8V8Ttq1hNYUa09qM-m-50j-rxhuq1XwJ1hA8J3Sum70bo4_JQVSS19COPaLTYneA=s900-c-k-c0x00ffffff-no-rj")
-      .setTitle(message.author.toString() + "의 지갑")
+      .setTitle("당신의 지갑")
       .setColor(0x00FF00)
       .setDescription(`${user.money} 💵`)
       .setThumbnail("https://picsum.photos/512/512")
       .setTimestamp(new Date())
+      .setFooter(`${user.name}`)
       
       msg.channel.send(embed);
     }else {
       const embed = new Discord.MessageEmbed()
       .setAuthor("초록봇", "https://yt3.ggpht.com/J8ldYsbmlo8V8Ttq1hNYUa09qM-m-50j-rxhuq1XwJ1hA8J3Sum70bo4_JQVSS19COPaLTYneA=s900-c-k-c0x00ffffff-no-rj")
-      .setTitle(message.author.toString() + "의 지갑")
+      .setTitle("당신의 지갑")
       .setColor(0xFE2E2E)
-      .setDescription(`비어있넹..`)
+      .setDescription(`0 💵`)
+      .setThumbnail("https://picsum.photos/512/512")
+      .setTimestamp(new Date())
+      .setFooter(`${user.name}`)
       
       msg.channel.send(embed);
     }
